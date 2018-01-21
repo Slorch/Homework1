28e56813771bbc328e5f5f2ce3a473efabe1102e
Brian Sun <Slorchthefirst@gmail.com>
2018-01-21 14:14:42 -0600

    final 1

---
+++ Homework1/hw1.cpp
@@ -0,0 +1,44 @@
+//First Homework answer
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	//initalize needed variables
+	string fname; //first name
+	string lname; //last name
+	int byear; //birth year
+	int fheight; //foot for height
+	int iheight; //inch for height
+	int age; //age in years
+	double cmheight; //height in cm
+	double avg_growth; //average growth
+
+	 //ask for information
+
+	//cout << "Enter first name: ";
+	cin >> fname; 
+
+	//cout << "Enter last name: ";
+	cin >> lname;
+
+	//cout << "Enter birth year: ";
+	cin >> byear;
+
+	//cout << "Enter your height foot: ";
+	cin >> fheight;
+
+	//cout << "Enter height in feet only: ";
+	cin >> iheight;
+
+	cmheight = (fheight * 12 + iheight) *  2.54;
+
+	age = 2018 - byear;
+	
+	avg_growth = (cmheight - 51) / age;
+
+	cout << age << endl;
+	cout << cmheight << endl;
+	cout << avg_growth << endl;
+	//system("pause");
+}
